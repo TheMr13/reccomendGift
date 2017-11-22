9dce52d0f33ce7aee8951bf86a2fb3521b7e18fd
Daniel <daniel@mcguinness.net.nz>
2017-11-23 09:34:49 +1300

    75

--- testScripts.js
+++ testScripts.js
@@ -7,50 +7,49 @@ function scrolling() {
 
 function posting() {
 
-    var custURL = "https://thealphatest.mybluemix.net/testing";
-    var message = "?userInput=" + document.getElementById("userInput").value
+    var scrollBox
 
-    userMessage();
-    scrollToBottom()
-    
-
-    //var response;
-
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            
-            var response = JSON.parse(this.responseText);
-
-            console.log(response.output.text[0]);
-
-            botReply()
-            scrollToBottom()
-        }
-    };
-
-    xhttp.open("GET", custURL + message);
-    xhttp.send(); 
-}     
-
-function userMessage() {
     var para = document.createElement("P");                       
     var t = document.createTextNode(document.getElementById("userInput").value);      
     para.appendChild(t);   
     para.setAttribute("id", "onTheLeft")                                     
     document.getElementById("chatBox").appendChild(para);
-}
-
-function botReply() {
-    var para = document.createElement("P");                       
-    var t = document.createTextNode(response.output.text[0]);      
-    para.appendChild(t);   
-    para.setAttribute("id", "onTheRight")                                     
-    document.getElementById("chatBox").appendChild(para);  
-}
-
-function scrollToBottom() {
-    var scrollBox;
-    scrollBox = document.getElementById("chatBox");
+    
+    scrollBox = document.getElementById("chatBox")
     scrollBox.scrollTop = scrollBox.scrollHeight;
-}
+
+var custURL = "https://thealphatest.mybluemix.net/testing";
+var message = "?userInput=" + document.getElementById("userInput").value
+
+var response;
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            
+            response = JSON.parse(this.responseText);
+
+            
+            console.log(response.output.text[0]);
+
+            var para = document.createElement("P");                       
+            var t = document.createTextNode(response.output.text[0]);      
+            para.appendChild(t);   
+            para.setAttribute("id", "onTheRight")                                     
+            document.getElementById("chatBox").appendChild(para);  
+
+            scrollBox = document.getElementById("chatBox")
+            scrollBox.scrollTop = scrollBox.scrollHeight;
+
+            
+
+        }
+    };
+
+
+    xhttp.open("GET", custURL + message);
+    xhttp.send();
+
+    
+}     
+
