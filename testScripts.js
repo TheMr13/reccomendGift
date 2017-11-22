43f294de433b59ad7d441b94d2fb47ee150169d3
Daniel <daniel@mcguinness.net.nz>
2017-11-22 13:33:28 +1300

    13

--- testScripts.js
+++ testScripts.js
@@ -1,6 +1,6 @@
 function posting() {
 var custURL = "https://thealphatest.mybluemix.net/testing";
-var message = "?userInput=" + document.getElementById("userInput")
+var message = "?userInput=" + document.getElementById("userInput").text
 
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
