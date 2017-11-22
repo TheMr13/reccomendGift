bfed7c8b35c07667c1d84c447c25379f54374a41
Daniel <daniel@mcguinness.net.nz>
2017-11-23 09:30:11 +1300

    74

--- testScripts.js
+++ testScripts.js
@@ -7,63 +7,50 @@ function scrolling() {
 
 function posting() {
 
-    var scrollBox
+    var custURL = "https://thealphatest.mybluemix.net/testing";
+    var message = "?userInput=" + document.getElementById("userInput").value
 
-    var para = document.createElement("P");                       
-    var t = document.createTextNode(document.getElementById("userInput").value);      
-    para.appendChild(t);   
-    para.setAttribute("id", "onTheLeft")                                     
-    document.getElementById("chatBox").appendChild(para);
+    userMessage();
+    scrollToBottom()
     
-    scrollBox = document.getElementById("chatBox")
-    scrollBox.scrollTop = scrollBox.scrollHeight;
 
-var custURL = "https://thealphatest.mybluemix.net/testing";
-var message = "?userInput=" + document.getElementById("userInput").value
-
-var response;
+    //var response;
 
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             
-            response = JSON.parse(this.responseText);
+            var response = JSON.parse(this.responseText);
 
-            
             console.log(response.output.text[0]);
 
-            var para = document.createElement("P");                       
-            var t = document.createTextNode(response.output.text[0]);      
-            para.appendChild(t);   
-            para.setAttribute("id", "onTheRight")                                     
-            document.getElementById("chatBox").appendChild(para);  
-
-            scrollBox = document.getElementById("chatBox")
-            scrollBox.scrollTop = scrollBox.scrollHeight;
-
-            
-
+            botReply()
+            scrollToBottom()
         }
     };
 
-
     xhttp.open("GET", custURL + message);
-    xhttp.send();
-
-    
+    xhttp.send(); 
 }     
 
-
-/*
-if (count == 0){
-    align = "onTheLeft"
-} else {
-    align = "onTheRight"
+function userMessage() {
+    var para = document.createElement("P");                       
+    var t = document.createTextNode(document.getElementById("userInput").value);      
+    para.appendChild(t);   
+    para.setAttribute("id", "onTheLeft")                                     
+    document.getElementById("chatBox").appendChild(para);
 }
 
-var para = document.createElement("P");                       // Create a <p> element
-var t = document.createTextNode("This is a paragraph.");      // Create a text node
-para.appendChild(t);   
-para.setAttribute("id", align)                                       // Append the text to <p>
-document.getElementById("some").appendChild(para);           // Append <p> to <div> with id="myDIV"
-*/
+function botReply() {
+    var para = document.createElement("P");                       
+    var t = document.createTextNode(response.output.text[0]);      
+    para.appendChild(t);   
+    para.setAttribute("id", "onTheRight")                                     
+    document.getElementById("chatBox").appendChild(para);  
+}
+
+function scrollToBottom() {
+    var scrollBox;
+    scrollBox = document.getElementById("chatBox");
+    scrollBox.scrollTop = scrollBox.scrollHeight;
+}
